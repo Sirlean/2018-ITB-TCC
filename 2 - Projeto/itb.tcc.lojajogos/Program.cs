17bbc2dd152688f7795b582195cb90b3c5194314
Sirlean Souza <sirleansantos_@hotmail.com>
2018-08-12 23:39:07 -0300

    Versão 1

---
+++ 2 - Projeto/itb.tcc.lojajogos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Itb.Tcc.LojaJogos
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var cifrado = ClnUtil.Cifrar("jogos");
+            var decifrado = ClnUtil.Decifrar(cifrado);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+        }
+    }
+}
